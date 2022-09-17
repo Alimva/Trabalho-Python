d28538b59d9719bccc1846cca868bc5a32fb36ed
faval99 <fabian.valero9911@gmail.com>
2022-09-17 09:36:22 -0300

    login pronto

--- carrinho.py
+++ carrinho.py
@@ -1,68 +1,64 @@
-import os
-
-produtos = (
-    {'id': 1, 'nome': 'produto1', 'preço': 100 },
-    {'id': 2, 'nome': 'produto2', 'preço': 200 },
-    {'id': 3, 'nome': 'produto3', 'preço': 50 },
-    {'id': 4, 'nome': 'produto4', 'preço': 150 }
-)
-
-carrinho = []
-
-def exibir_opcoes():
-    print('\n\n')
-    
-    print('1 - Adicionar item ao carrinho')
-    print('2 - Remover item do carrinho')
-    print('3 - Exibir itens e o valor total a pagar')
-    print('4 - Limpar carrinho')
-    print('5 -Sair')
-
-def exibir_produtos():
-    for p in produtos:
-        print('Id: {0} - Nome: {1} - Preço: {2}\n'.format(p['id'],p['nome'], p['preço']))
-
-opcao = '1'
-
-os.system('clear')
-print('Carrinho de compras \n')
-
-def obter_nome_produto(id):
-    for p in produtos:
-        if p['id'] == id:
-            return p['nome']
-
-while opcao != "5":
-    exibir_opcoes()
-    opcao = input('Digite uma opção: ')     
-
-    if opcao == '1':
-        exibir_produtos()
-        
-        id = int(input('Digite o número do produto:'))
-        quantidade = int(input('Digite a quantidade desejada do produto: '))
-        carrinho.append({'id': id, 'quantidade': quantidade})
-
-    if opcao == '2':
-        id = int(input('digite o número do produto:'))
-        temp = []
-        for item in carrinho:
-            if item['id'] != id:
-                temp.append(item)
-    
-    if opcao == '3':
-        print('\n\n')
-        somatorio = 0 
-        for item in carrinho:
-            for produto in produtos:
-                if produto['id'] == item['id']:
-                    somatorio = somatorio + \
-                        (produto['preço'] * item['quantidade'])
-                    break
-            
-            print(
-                'Nome: {0} - Quantidade: {1}'.format(obter_nome_produto(item['id']), item['quantidade']))
-        print('Preço total: {0}'.format(somatorio))
-    
-    if opcao == '4':
-        carrinho = []                
+#from asyncio.proactor_events import _ProactorDuplexPipeTransport
+#import os
+#from tabnanny import check
+#
+#
+#class Carrinho:
+#
+#    def __init__(self,produtos,tamanho,cor,quantidade):
+#        self.produtos = produtos
+#        self.tamanho = tamanho
+#        self.cor = cor
+#        self.quantidade = quantidade
+#        
+#
+#    preco = {'Tênis Run Max':200,'Bota de Caminhada':350, 'Sandália Confort':250, 'Sapatilha de Escalada':500}
+#
+#    def carrinho(self):
+#        count = 0
+#        while count == 0:
+#            
+#            print('\n\n')
+#            print('1 - Remover item do carrinho')
+#            print('2 - Exibir itens e o valor total a pagar')
+#            print('3 - Limpar carrinho')
+#            print('4 -Sair')
+#            
+#            check = input('Escolha uma opção: ')
+#            print('\n')
+#
+#            if(check=='1'):
+#                #removendo itens do carrinho
+#                for i in self.produtos:
+#                    remover = input("Que item deseja remover?")
+#
+#                    if remover in self.produtos:
+#                        self.produtos= self.produtos.remove(remover)
+#                        break
+#
+#          
+#
+#            elif(check == '2'):
+#                #exibir itens e mostrar o valor total a pagar
+#                valor = 0
+#                for i in range(len(self.produto)):
+#                    print('Produtos no carrinho')
+#                    print(self.produto[i] + self.tamanho[i] + self.cor[i],+ self.quantidade[i])
+#                    print('O valor total a ser pago será de: ',valor)
+#
+#                #imprimindo o valor total    
+#                preco = {'Tênis Run Max': 200,'Bota de Caminhada': 350, 'Sandália Confort': 250, 'Sapatilha de Escalada': 500}
+#                for nome in self.produtos:
+#                    valor += preco[nome]
+#                return valor
+#                    
+#            elif(check == '3'):
+#                Carrinho = []
+#                print('O carrinho está limpo.')
+#
+#            elif(check == '4'):
+#                self.out = 4
+#                return self.out
+#
+#            else:
+#                print('Intente novamente. Digite uma opção válida.')    
--- login.py
+++ login.py
@@ -6,59 +6,62 @@ from sre_constants import SUCCESS
 from unicodedata import name
 import json
 
-granted = False
-def grant():
-    global granted
-    granted = True
-
-#Fazendo login
-def login(nome,senha):
-    SUCCESS = False
-    file = open("DadosUsuarios.json","r")
-    for i in file:
-        
-        a,b = i.split(",")
-        b = b.strip()
-        if(a == nome and b == senha):
-            SUCCESS = True
-            break
-    file.close()
+class Login:
+    granted = False
+    def grant():
+        global granted
+        granted = True
     
-    if(SUCCESS):
-        print("Login realizado com sucesso!!!")
+    #Fazendo login
+    def login(nome,senha):
+        SUCCESS = False
+        file = open("DadosUsuarios.json","r")
+        for i in file:
+            
+            a,b = i.split(",")
+            b = b.strip()
+            if(a == nome and b == senha):
+                SUCCESS = True
+                break
+        file.close()
+        
+        if(SUCCESS):
+            print("Login realizado com sucesso!!!")
+            grant()
+        else:
+            print("Nome de usuário ou senha são incorretos! Tenta novamente.")    
+    
+    #Função para realizar cadastro
+    def cadastro(nome,senha):
+        file = open("DadosUsuarios.json","a")
+        file.write(nome + "," + senha + "\n")
+        file.close()
         grant()
-    else:
-        print("Nome de usuário ou senha são incorretos! Tenta novamente.")    
-
-#Função para realizar cadastro
-def cadastro(nome,senha):
-    file = open("DadosUsuarios.json","a")
-    file.write(nome + "," + senha + "\n")
-    file.close()
-    grant()
-
-
-def access(opcion):
-    global nome
-    if(option == "login"):
-        nome = input("Digite seu nome: ")
-        senha = input("Digite sua senha: ")
-        login(nome,senha)
-    else:
-        print("Por favor, digite seu nome e senha para fazer cadastro")
-        nome = input("Digite seu nome: ")
-        senha = input("Digite sua senha: ")
-        cadastro(nome,senha)
-
-def begin():
-    global option
-    print('##########  Login  ##########')
-    option = input('Deseja fazer Login ou Cadastro? \n Por favor, digite uma opção (login, cadastro) :')
-    if (option != "login" and option != "cadastro"):
-        begin()
-begin()        
-access(option)
-if(granted):
-    print("Seja Bem-Vindo!")
-    print("### Detalhes de Usuário ###")
-    print("Nome de usuário: ",nome)
+    
+    
+    def access(opcion):
+        global nome
+        if(option == "login"):
+            nome = input("Digite seu nome: ")
+            senha = input("Digite sua senha: ")
+            login(nome,senha)
+        else:
+            print("Por favor, digite seu nome e senha para fazer cadastro")
+            nome = input("Digite seu nome: ")
+            senha = input("Digite sua senha: ")
+            cadastro(nome,senha)
+    
+    def begin():
+        global option
+        print('##########  Login  ##########')
+        option = input('Deseja fazer Login ou Cadastro? \n Por favor, digite uma opção (login, cadastro) :')
+        if (option != "login" and option != "cadastro"):
+            begin()
+            
+    begin()        
+    access(option)
+    if(granted):
+        print("Seja Bem-Vindo!")
+        print("### Detalhes de Usuário ###")
+        print("Nome de usuário: ",nome)
+    
